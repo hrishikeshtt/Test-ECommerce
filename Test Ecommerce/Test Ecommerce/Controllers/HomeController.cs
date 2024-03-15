3f6a81a8ed6e6e8e76a3c1b9ec54a433d9751b13
Hrishikesh T T <hrishikesh2606@gmail.com>
2024-03-15 12:27:59 +0530

    Add files via upload

---
+++ Test Ecommerce/Test Ecommerce/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_Ecommerce.Model;
+
+namespace Test_Ecommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Login loginUser)
+        {       
+            return View(loginUser.UserName);
+        }
+    }
+}
---
+++ Test Ecommerce/Test Ecommerce/Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test_Ecommerce
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
